42ba8efcdb4abc1a64e8072f30e8fb9820c33487
Julian Womack <Julian.Womack@berkeley.edu>
2026-06-16 16:47:23 -0700

    Initial commit: add Python practice files

---
+++ profile.py
@@ -0,0 +1,11 @@
+full_name = "Julian Womack Batzdorf"
+nickname = "Ju"
+age = 22
+used_python_before = False
+hobbies = [  "snowboarding", "guitar", "football", "videograpghy"]
+favorites = {"movie": "up", "food": "tacos", "hobby": "football"}
+skills = ("video editing" , "science", "teamwork")
+languages_spoken = {"English", "German"}
+
+
+print(full_name, age, hobbies)
